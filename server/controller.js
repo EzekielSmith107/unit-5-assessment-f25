72d183fc7d538faf9858d5e30d9dbb01fa9d753c
Ezekiel Smith <ezesmith107@gmail.com>
2022-07-15 12:19:12 -0400

    Step 2 complete

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,16 @@
+requestAnimationFrame("dotenv").config()
+const Sequelize = require("sequelize")
 
+const{CONNECTION_STRING} = process.env
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
 
 module.exports = {
     seed: (req, res) => {
